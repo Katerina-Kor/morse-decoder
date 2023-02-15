8e3afed6f6e6a5a52fd3bbe306f8ab012a4a0728
Katerina-Kor <katerina.korobeiko@gmail.com>
2023-02-15 15:46:14 +0400

    feat: add the function body

--- src/index.js
+++ src/index.js
@@ -38,7 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    for (let i = 0; i < expr.length; i += 2) {
+        let sign = expr.slice(i, i + 2);
+        sign == '10' ? arr.push('.') : sign == '11' ? arr.push('-') : arr.push(sign);
+    };
+
+    let matrix = [];
+    for (let i = 0; i < arr.length; i += 5) {
+        matrix.push(arr.slice(i, i + 5));
+    };
+
+    let morseArr = matrix.map(item => item.filter(sign => sign !== '00').join(''));
+
+    let word = '';
+    morseArr.forEach(item => {
+        item == '**********' ? word += ' ' : word += MORSE_TABLE[item];
+    });
+    return word;
 }
 
 module.exports = {
